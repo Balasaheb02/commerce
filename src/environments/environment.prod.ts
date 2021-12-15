793abe460f7ecc3184cc91d242bfbbf2367946d5
Balasaheb thakare <balasaheb.thakare@enrichai.com>
2021-12-15 18:16:47 +0530

    added global url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://606c8faac445570017a47685.mockapi.io'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://606c8faac445570017a47685.mockapi.io'
 };
 
 /*
