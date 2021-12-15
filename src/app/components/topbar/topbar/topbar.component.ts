36af1710d8ecca544617b4fd8ddbb306058d0920
Balasaheb thakare <balasaheb.thakare@enrichai.com>
2021-12-15 18:18:04 +0530

    added topbar module and component

---
+++ src/app/components/topbar/topbar/topbar.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+
+
+@Component({
+  selector: 'app-topbar',
+  templateUrl: './topbar.component.html',
+  styleUrls: ['./topbar.component.scss']
+})
+export class TopbarComponent implements OnInit {
+
+  @Output() toggleButton = new EventEmitter<boolean>(false);
+  @Output() filter = new EventEmitter<boolean>(false);
+  toggleValue = false;
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  toggleSidebar(value: boolean) {
+    this.toggleValue = !this.toggleValue;
+    this.toggleButton.emit(this.toggleValue);
+  }
+
+  openFilter() {
+    this.filter.emit(true);
+  }
+
+  
+}
